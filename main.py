6913cfa6bfbb671c4e2596dda61b932e6e3bc3aa
RucatJ <ruairijackman@gmail.com>
2026-06-06 11:02:09 +0100

    initialize particles

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
+import numpy as np
+
+world_width = 600
+world_height = 400
+n = 10
 
 pygame.init()
 
-screen = pygame.display.set_mode((600,400))
+screen = pygame.display.set_mode((world_width,world_height))
 
 running = True
+
+# Initialize arrays with random values
+
+positions = np.column_stack([
+    np.random.uniform(0, world_height, n),
+    np.random.uniform(0, world_width, n)
+])
+print(positions)
+
+velocities = np.zeros((n, 2))
+print(velocities)
 
 # Running loop
 while running:
