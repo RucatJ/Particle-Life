13a676df836ac157e6a034ca57650419432d0038
RucatJ <ruairijackman@gmail.com>
2026-06-07 10:28:33 +0100

    optimize with grid and by reducing square root use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import numpy as np
-from math import floor
+from math import floor, ceil
 
 world_width = 800
 world_height = 800
@@ -9,12 +9,12 @@ screen_width = 1000
 screen_height = 1000
 
 # Interaction limits
-max_dist = 200
+max_dist = 150
 min_dist = 10
 
 camera = [(screen_width - world_width) / 2, (screen_height - world_height) / 2]
 
-n = 250
+n = 500
 background_colour = (20,20,40)
 world_colour = (40, 40, 80,)
 r = 3
@@ -96,63 +96,64 @@ while running:
 
     # Create grid and add all particles to grid
 
-    grid = [[[] for i in range(floor(world_width / max_dist))] for i in range(floor(world_height / max_dist))]
+    grid = {}
+
+    # print(grid)
 
     for i, p in enumerate(particles):
         cell_x = floor(p[0] / max_dist)
         cell_y = floor(p[1] / max_dist)
-        if not grid[cell_y][cell_x] == 0:
-            grid[cell_y][cell_x].append(i)
-        else:
-            grid[cell_y][cell_x] = [i]
 
-        p[5] = cell_x
-        p[6] = cell_y
+        # print(p[0], cell_x, p[1], cell_y)
+
+        grid.setdefault((cell_y, cell_x), []).append(i)
+
+        p[5] = cell_y
+        p[6] = cell_x
 
     # Particle interactions (where it all goes wrong)
     for p in particles:
-        near = []
-
         for dy in (-1, 0, 1):
-            if grid[dy]:
                 for dx in (-1, 0 ,1):
-                    if grid[dy][dx]:
-                        cell = (int(p[6] + dx), int(p[5] + dy))
-                        near.append(cell)
+                    if (int(p[5] + dy), int(p[6] + dx)) in grid.keys():
+                        for i in grid[(int(p[5] + dy), int(p[6] + dx))]:
+                            _p = particles[i]
 
-        print(near)
+                            force = [0, 0]
 
-        for _p in particles:
-            force = [0, 0]
+                            diff_vect = [p[0] - _p[0], p[1] - _p[1]]
+                            square_diff = diff_vect[0]**2 + diff_vect[1]**2
 
-            diff_vect = [p[0] - _p[0], p[1] - _p[1]]
-            diff = np.linalg.norm(diff_vect)
+                            if not square_diff:
+                                continue
 
-            if not diff:
-                continue
-            norm = [diff_vect[0] / diff, diff_vect[1] / diff]
+                            # print(diff)
+                            if not max_dist**2 <= square_diff and not min_dist**2 >= square_diff:
+                                diff = np.linalg.norm(diff_vect)
+                                norm = [diff_vect[0] / diff, diff_vect[1] / diff]
 
-            # print(diff)
-            if not max_dist <= diff and not min_dist >= diff:
-                force = force_matrix[int(p[4])][int(_p[4])] * ((max_dist/2 - abs(max_dist/2 - (diff - min_dist)))/(max_dist/2))
-                force = [force * norm[0], force * norm[1]]
-                # print(force)
+                                force = force_matrix[int(p[4])][int(_p[4])] * ((max_dist/2 - abs(max_dist/2 - (diff - min_dist)))/(max_dist/2))
+                                force = [force * norm[0], force * norm[1]]
+                                # print(force)
 
-                # print(force)
-            elif diff <= min_dist:
-                force = -pow((diff - min_dist)/2, 2)
-                force = [force * norm[0], force * norm[1]]
+                                # print(force)
+                            elif square_diff <= min_dist**2:
+                                diff = np.linalg.norm(diff_vect)
+                                norm = [diff_vect[0] / diff, diff_vect[1] / diff]
 
-            _p[2] += force[0] * force_scale
-            _p[3] += force[1] * force_scale
+                                force = -((diff - min_dist)/2)**2
+                                force = [force * norm[0], force * norm[1]]
+
+                            _p[2] += force[0] * force_scale
+                            _p[3] += force[1] * force_scale
 
     # Update particle positions
     particles[:, 0] += particles[:, 2]
     particles[:, 1] += particles[:, 3]
 
     # Slightly dampen velocities over time
-    particles[:, 2] *= 0.9
-    particles[:, 3] *= 0.9
+    particles[:, 2] *= 0.95
+    particles[:, 3] *= 0.95
 
     # Update screen
     pygame.display.update()
