fed0c6d473636e395c79166b9e8aabfd594b64b5
RucatJ <ruairijackman@gmail.com>
2026-06-07 08:22:55 +0100

    add minor optimizations and replace arrays with one big particles array

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import pygame
 import numpy as np
-from numpy.random import normal
 
-world_width = 400
-world_height = 400
+world_width = 500
+world_height = 500
 
-screen_width = 600
-screen_height = 600
+screen_width = 700
+screen_height = 700
 
 # Interaction limits
-max_dist = 200
-min_dist = 20
+max_dist = 250
+min_dist = 5
 
-camera = [world_height/2, world_height/2]
+camera = [world_width/2, world_height/2]
 
-n = 150
+n = 200
 background_colour = (20,20,40)
 world_colour = (40, 40, 80,)
-r = 5
+r = 3
 
-type_count = 4
+type_count = 8
 colours = np.random.uniform(0,255, (type_count, 3))
 
 pygame.init()
@@ -28,31 +27,26 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 
 running = True
 
-# Initialize arrays with random values
-
-positions = np.column_stack([
-    np.random.uniform(0, world_width, n),
-    np.random.uniform(0, world_height, n)
-])
-# print(positions)
-
-# Initialize types matrix
-
-types = np.random.randint(0, type_count, n)
-print(types)
-
-# velocities = np.zeros((n, 2))
-
-velocities = np.column_stack([
-    np.random.uniform(-1, 1, n),
-    np.random.uniform(-1, 1, n)
-])
-
 # Initialize the coolest sounding array name ever
 
 force_matrix = np.random.uniform(-1, 1, (type_count, type_count))
 
 print(force_matrix)
+
+# Initialize all particles
+
+particles = np.column_stack([
+    # Positions
+    np.random.uniform(0, world_width, n),
+    np.random.uniform(0, world_height, n),
+    # Velocities
+    np.random.uniform(-1, 1, n),
+    np.random.uniform(-1, 1, n),
+    # Types
+    np.random.randint(0, type_count, n)
+])
+
+print(particles)
 
 clock = pygame.time.Clock()
 
@@ -76,45 +70,53 @@ while running:
     ])
 
     # Clamp particle positions
-    positions[:, 0] = np.clip(positions[:, 0], r, world_width - r)
-    positions[:, 1] = np.clip(positions[:, 1], r, world_height - r)
+    particles[:, 0] = np.clip(particles[:, 0], r, world_width - r)
+    particles[:, 1] = np.clip(particles[:, 1], r, world_height - r)
 
     # Draw particles
-    for i, p in enumerate(positions):
+    for p in particles:
+        # print(p, camera)
         position = (
             int(p[0] + camera[0]/2),
             int(p[1] + camera[1]/2)
         )
 
-        pygame.draw.circle(screen, colours[types[i]-1], position, r)
+        pygame.draw.circle(screen, colours[int(p[4])-1], position, r)
+        pygame.draw.circle(screen, (255, 255, 255), camera, 10)
 
     # Particle interactions (where it all goes wrong)
-    for i, p in enumerate(positions):
-        for _i, _p in enumerate(positions):
-            diff_vect = np.array([p[0], p[1]]) - [_p[0], _p[1]]
+    for p in particles:
+        for _p in particles:
+            force = [0, 0]
+
+            diff_vect = [p[0] - _p[0], p[1] - _p[1]]
             diff = np.linalg.norm(diff_vect)
-            # print(diff)
-            if diff == 0:
+
+            if not diff:
                 continue
+            norm = [diff_vect[0] / diff, diff_vect[1] / diff]
+
+            # print(diff)
             if not max_dist <= diff and not min_dist >= diff:
-                A = force_matrix[types[i]][types[_i]]
-                # print(A)
-                X = max_dist
-                N = min_dist
-                R = diff
-
-                force = A * ((X/2 - abs(X/2 - (R - N)))/(X/2)) * (diff_vect / diff)
+                force = force_matrix[int(p[4])][int(_p[4])] * ((max_dist/2 - abs(max_dist/2 - (diff - min_dist)))/(max_dist/2))
+                force = [force * norm[0], force * norm[1]]
                 # print(force)
 
                 # print(force)
-                _p[0] += force[0]
-                _p[1] += force[1]
+            elif diff <= min_dist:
+                force = -pow((diff - min_dist)/2, 2)
+                force = [force * norm[0], force * norm[1]]
+
+            _p[0] += force[0]
+            _p[1] += force[1]
 
     # Update particle positions
-    positions += velocities
+    particles[:, 0] += particles[:, 2]
+    particles[:, 1] += particles[:, 3]
 
     # Slightly dampen velocities over time
-    velocities *= 0.995
+    particles[:, 2] *= 0.995
+    particles[:, 3] *= 0.995
 
     # Update screen
     pygame.display.update()
